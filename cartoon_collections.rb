89c7a31c520f752b5a096840517376f17c018545
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-05 21:49:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types).find? {|element| element ? element : nil}
+  array.find(cheese_types) {|element| element ? element : nil}
 end
